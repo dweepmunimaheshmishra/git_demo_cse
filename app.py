c692d51a05d39b4c9fed773bbad9eabc25483196
dweepmunimaheshmishra <dweepmunimaheshmishra@gmail.com>
2026-02-06 11:01:08 +0530

    Implement basic arithmetic functions in app.py
    Added basic arithmetic functions: add, sub, mul, and div. Fixed main block to print results of calculations.

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+def add(a,b):
+  return a+b
+def sub(a,b):
+  return a-b
+def mul(a,b):
+  return a*b
+def div(a,b):
+  if b==0:
+    return "cant have 0 as value"
+  return a/b
 
+if _name_=="_main_":
+  print(calcultae app")
+  print("10+5" = {add(10,5)}")
+  print("10+5" = {sub(10,5)}")
+  print("10+5" = {div(10,5)}")
+  print("10+5" = {mul(10,5)}")
+  
